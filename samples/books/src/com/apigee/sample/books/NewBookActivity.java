f8506e8a54ff1022d8fce92981fdde863ea29a9a
Paul Dardeau <pauldardeau@me.com>
2013-07-22 15:28:52 -0500

    USERGRID-1726: added books sample

---
+++ samples/books/src/com/apigee/sample/books/NewBookActivity.java
@@ -0,0 +1,76 @@
+package com.apigee.sample.books;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.apigee.sample.books.R;
+
+import com.apigee.sdk.data.client.DataClient;
+import com.apigee.sdk.data.client.callbacks.ApiResponseCallback;
+import com.apigee.sdk.data.client.response.ApiResponse;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NewBookActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_book);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_book, menu);
+		return true;
+	}
+
+	public void createBook(View view){
+		BooksApplication bookApp = (BooksApplication) getApplication();
+		DataClient client = bookApp.getDataClient();
+		
+		if (client != null) {
+			EditText title = (EditText)findViewById(R.id.title);
+			String bookTitle = title.getText().toString();
+		
+			EditText author = (EditText)findViewById(R.id.author);
+			String bookAuthor = author.getText().toString();
+		
+			Map<String, Object> entity = new HashMap<String,Object>();
+			entity.put("type", "books");
+			entity.put("author", bookAuthor);
+			entity.put("title", bookTitle);
+		
+			client.createEntityAsync(entity, new ApiResponseCallback(){
+				@Override
+				public void onException(Exception ex) {
+					Log.i("NewBook", ex.getMessage());
+				}
+			
+				@Override
+				public void onResponse(ApiResponse response) {
+					finish();		
+				}
+			});
+		
+		} else {
+			Log.d("Books",BooksApplication.apigeeNotInitializedLogError);
+			
+			Context context = getApplicationContext();
+			CharSequence text = "Apigee client is not initialized";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		}
+		
+	}
+}
