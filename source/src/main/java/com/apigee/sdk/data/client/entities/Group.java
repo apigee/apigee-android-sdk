f5ceaf85d34186787b09823577b9a914b3daccf3
amuramoto <amuramoto@apigee.com>
2014-04-01 15:01:56 -0700

    javadoc for Group.java

--- source/src/main/java/com/apigee/sdk/data/client/entities/Group.java
+++ source/src/main/java/com/apigee/sdk/data/client/entities/Group.java
@@ -16,19 +16,41 @@ public class Group extends Entity {
 	public final static String PROPERTY_PATH  = "path";
 	public final static String PROPERTY_TITLE = "title";
 
+	/**
+	 * Checks if the provided 'type' equals 'group'.
+	 *
+	 * @param  type  the type to compare
+	 * @return  Boolean true/false
+	 */
 	public static boolean isSameType(String type) {
 		return type.equals(ENTITY_TYPE);
 	}
 
+	/**
+	 * Default constructor for the Group object. Sets the 'type'
+	 * property to 'group'.	 
+	 */
 	public Group() {
 		setType(ENTITY_TYPE);
 	}
 	
+	/**
+	 * Constructs the Group object with a DataClient. Sets the 'type'
+	 * property to 'group'.
+	 *
+	 * @param dataClient an instance of DataClient
+	 */
 	public Group(DataClient dataClient) {
 		super(dataClient);
 		setType(ENTITY_TYPE);
 	}
 
+	/**
+	 * Constructs the Group object from an Entity object. If the 'type'
+	 * property of the Entity is not 'group' it will be overwritten.
+	 *
+	 * @param  entity  an Entity object
+	 */
 	public Group(Entity entity) {
 		super(entity.getDataClient());
 		properties = entity.properties;
@@ -37,12 +59,24 @@ public class Group extends Entity {
 
 	@Override
 	@JsonIgnore
+	/**
+	 * Returns the valye of the 'type' property of the Group object.
+	 * Should always be 'group'.
+	 *
+	 * @return  the String 'group'
+	 */
 	public String getNativeType() {
 		return ENTITY_TYPE;
 	}
 
 	@Override
 	@JsonIgnore
+	/**
+	 * Gets all the current property names in the Group object and adds
+	 * the 'path' and 'title' properties.
+	 *
+	 * @return  a List object that contains the properties list
+	 */
 	public List<String> getPropertyNames() {
 		List<String> properties = super.getPropertyNames();
 		properties.add(PROPERTY_PATH);
@@ -51,19 +85,39 @@ public class Group extends Entity {
 	}
 
 	@JsonSerialize(include = NON_NULL)
+	/**
+	 * Gets the value of the 'path' property of the Group object.
+	 *
+	 * @return  the value of the 'path' property
+	 */
 	public String getPath() {
 		return getStringProperty(PROPERTY_PATH);
 	}
 
+	/**
+	 * Sets the value of the 'path' property of the Group object.
+	 *
+	 * @param  the value of the 'path' property
+	 */
 	public void setPath(String path) {
 		setStringProperty(properties, PROPERTY_PATH, path);
 	}
 
+	/**
+	 * Gets the value of the 'title' property of the Group object.
+	 *
+	 * @return  the value of the 'title' property
+	 */
 	@JsonSerialize(include = NON_NULL)
 	public String getTitle() {
 		return getStringProperty(PROPERTY_TITLE);
 	}
 
+	/**
+	 * Sets the value of the 'title' property of the Group object.
+	 *
+	 * @return  the value of the 'title' property
+	 */
 	public void setTitle(String title) {
 		setStringProperty(properties, PROPERTY_TITLE, title);
 	}
