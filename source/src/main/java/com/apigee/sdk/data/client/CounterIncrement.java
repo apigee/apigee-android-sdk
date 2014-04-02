3bc9c4f78ffd6af184737eaa8ba9059a156c7bab
Steven Traut <straut@apigee.com>
2014-04-01 20:08:14 -0700

    A bit of work on event and collection management.

--- source/src/main/java/com/apigee/sdk/data/client/CounterIncrement.java
+++ source/src/main/java/com/apigee/sdk/data/client/CounterIncrement.java
@@ -1,15 +1,35 @@
 package com.apigee.sdk.data.client;
 
+/**
+ * Represents an incrementing counter, including the counter's
+ * name and increment value.
+ * 
+ * You can use a counter when creating a number of similar 
+ * Event entity instances. An Event entity's "counters" property
+ * is a Map in which the key is the counter name and value is the 
+ * increment value.
+ */
 public class CounterIncrement {
 
 	private String counterName;
 	private long counterIncrementValue;
 	
 	
+	/**
+	 * Constructs an instance, specifying the increment value
+	 * as 1.
+	 */
 	public CounterIncrement() {
 		this.counterIncrementValue = 1;
 	}
-	
+
+	/**
+	 * Constructs an instance with the specified counter name
+	 * and increment value.
+	 * 
+	 * @param counterName The counter name.
+	 * @param counterIncrementValue The counter's increment value.
+	 */
 	public CounterIncrement(String counterName, long counterIncrementValue) {
 		this.counterName = counterName;
 		this.counterIncrementValue = counterIncrementValue;
