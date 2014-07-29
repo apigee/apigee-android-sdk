4455b5d999003a762335826d77c5ffc6ff0fa56c
Robert Walsh <rjwalsh1985@gmail.com>
2014-07-29 12:58:32 -0700

    Initial commit for junit testing.

---
+++ source/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ApigeeCorporation on 7/29/14.
+ */
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+    @org.junit.Test public void sampleTest() {
+        assertEquals("Sample Test", 1, 1);
+    }
+}
