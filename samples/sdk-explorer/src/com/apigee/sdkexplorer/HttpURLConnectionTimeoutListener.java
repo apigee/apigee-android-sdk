8738ee87ef0ffcefc42484f033ff0c089b3170be
Robert Walsh <rjwalsh1985@gmail.com>
2014-09-21 18:39:44 -0700

    Deleting the old sample applications.

--- samples/sdk-explorer/src/com/apigee/sdkexplorer/HttpURLConnectionTimeoutListener.java
+++
@@ -1,5 +0,0 @@
-package com.apigee.sdkexplorer;
-
-public interface HttpURLConnectionTimeoutListener {
-	public void notifyOnConnectionTimeout();
-}
--- samples/sdk-explorer/src/com/apigee/sdkexplorer/NetworkResponseListener.java
+++
@@ -1,7 +0,0 @@
-package com.apigee.sdkexplorer;
-
-public interface NetworkResponseListener
-{
-	public void notifyNetworkResponseSuccess(String response);
-	public void notifyNetworkResponseFailure(Exception exception,String response);
-}
