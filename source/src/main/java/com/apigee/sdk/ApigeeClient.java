572a2d74a1f615bc392a83e52f734aee3308aeec
Robert Walsh <rjwalsh1985@gmail.com>
2015-01-12 11:13:48 -0800

    Updated to version 2.0.14

--- source/src/main/java/com/apigee/sdk/ApigeeClient.java
+++ source/src/main/java/com/apigee/sdk/ApigeeClient.java
@@ -25,7 +25,7 @@ public class ApigeeClient {
 	/**
      * Most current version of the Apigee Android SDK
      */
-    public static final String SDK_VERSION  = "2.0.13";
+    public static final String SDK_VERSION  = "2.0.14";
     /**
      * Platform type of this SDK
      */
